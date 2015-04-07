e5a402158d4b53010dc5c1e4dcd9cd5cac878ab7
kyhfan <yh.kim@minivertising.kr>
2015-04-07 15:31:02 +0900

    기본 레이아웃

--- PC/index.php
+++ PC/index.php
@@ -27,13 +27,15 @@
       </div>
       <!------------- 첫번째 영역 -------------------->
       <!------------- 두번째 영역 -------------------->
-      <div class="area2 " style="width:100%;height:995px;">
-        <center>
-          <h3>거리에서 만나는 즐거움 하나</h3>
-          <h1>수분을 지켜라, 즐거움이 터진다!</h1>
-          <h4>00월부터 전국 곳곳에서 당신을 촉촉하게 할 즐거움이 찾아갑니다.</h4>
-          <h4>이벤트에 참여해 당신의 수분을 지키고, 다양한 선물도 받아가세요!.</h4>
-        </center>
+      <div class="area2" style="width:100%;height:995px;">
+        <div>
+          <center>
+            <h3>거리에서 만나는 즐거움 하나</h3>
+            <h1>수분을 지켜라, 즐거움이 터진다!</h1>
+            <h4>00월부터 전국 곳곳에서 당신을 촉촉하게 할 즐거움이 찾아갑니다.</h4>
+            <h4>이벤트에 참여해 당신의 수분을 지키고, 다양한 선물도 받아가세요!.</h4>
+          </center>
+        </div>
         <div class="scrollme" style="text-align:center;left:50%;margin-left:400px">
           참여방법<br />
           <div class="animateme" data-when="enter" data-from="1" data-to="0" data-opacity="0" data-translatey="200" style="width:200px;float:left;text-align:center;">
@@ -55,9 +57,28 @@
       </div>
       <!------------- 두번째 영역 -------------------->
       <!------------- 세번째 영역 -------------------->
-      <div class="area3">
+      <div class="area3" style="width:100%;height:995px;">
+        <div>
+          <center>
+            <h3>거리에서 만나는 즐거움 둘</h3>
+            <h1>달콤한 캔디카를 만나보세요!</h1>
+          </center>
+        </div>
+        <div>
+          <a href="#">자세히 보기</a>
+        </div>
       </div>
       <!------------- 세번째 영역 -------------------->
+      <!------------- 네번째 영역 -------------------->
+      <div class="area4" style="width:100%;height:995px;">
+        <div>
+          <a href="#">모이스쳐밤 자세히 보기</a>
+        </div>
+        <div>
+          <a href="#">아쿠아밤 자세히 보기</a>
+        </div>
+      </div>
+      <!------------- 네번째 영역 -------------------->
     </div>
   </body>
 </html>
