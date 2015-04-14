7e9d09cccd412d04783220668c5898697c2536cc
Seonhye yang <yangseonhye@yangseonhyeui-iMac.local>
2015-04-14 16:18:56 +0900

    PC index업로드

--- PC/header.php
+++ PC/header.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" type="text/css" href="../lib/Magnific-Popup/magnific-popup.css"> 
     <link rel="stylesheet" href="../lib/iCheck/skins/all.css">
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style_yang.css" />
 
     <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
@@ -31,10 +32,6 @@
     <script type='text/javascript' src='../lib/iCheck/icheck.js'></script>
     <script type="text/javascript" src="../js/jQueryTween-aio-min.js"></script>
     <!-- <script type="text/javascript" src="../js/jquery.scrollme.min.js"></script> -->
-
-	<style>
-	html,body { height: 100%; margin: 0; padding: 0; }
-    </style>
 
   </head>
   <body>
