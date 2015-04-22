826d69ecb69812d48af519820f21b55395ee322d
kyhfan <yh.kim@minivertising.kr>
2015-04-22 10:41:55 +0900

    카카오톡 공유

--- js/main.js
+++ js/main.js
@@ -1200,14 +1200,38 @@ function sns_share(media)
 {
 	if (media == "facebook")
 	{
-		var newWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('http://www.belifbomb.co.kr/?media=fb'),'sharer','toolbar=0,status=0,width=600,height=325');
+		var newWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('http://www.belifbomb.co.kr/?media=facebook'),'sharer','toolbar=0,status=0,width=600,height=325');
 		$.ajax({
 			type   : "POST",
 			async  : false,
 			url    : "../main_exec.php",
 			data:{
 				"exec" : "insert_share_info",
-				"media" : "facebook"
+				"media" : media
+			}
+		});
+	}else if (media == "kakao"){
+		// 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
+		Kakao.Link.createTalkLinkButton({
+		  container: '#kakao-link-btn',
+		  label: "서장훈이 화장품 CF를?! \n<아니 아니, 그게 아니고~> 전격 공개!\n 건조한 피부에 봄비같은 하얀 수분 크림 출시!\n 지금 10ml Kit도 신청하세요!",
+		  image: {
+			src: 'http://www.thefaceshopclouding.co.kr/PC/images/sns_kt.jpg',
+			width: '1200',
+			height: '630'
+		  },
+		  webButton: {
+			text: '더페이스샵',
+			url: 'http://www.belifbomb.co.kr/?media=kakao' // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
+		  }
+		});
+		$.ajax({
+			type   : "POST",
+			async  : false,
+			url    : "../main_exec.php",
+			data:{
+				"exec" : "insert_share_info",
+				"media" : media
 			}
 		});
 	}else{
