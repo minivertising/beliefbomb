0853fb895a3c735c9a0487a7000b64761a6bf081
kyhfan <yh.kim@minivertising.kr>
2015-04-24 17:09:34 +0900

    크론탭 적용

--- PC/send_sms.php
+++ PC/send_sms.php
@@ -1,7 +1,108 @@
 <?
-	include_once "../config.php";
+	$my_db = mysqli_connect("localhost", "root", "m!nv#Rtisin9", "belifbomb");
+	if (mysqli_connect_error()) {
+		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+	}
+	
+	include_once "include/global.php"; 			//변수정보
+	include_once "include/function.php"; 		//함수정보
 
 	send_lms("010-3003-3965","http://www.belif.co.kr");
+
+	// LMS 발송 
+	function send_lms($phone, $s_url)
+	{
+		global $_gl;
+		global $my_db;
+
+		$httpmethod = "POST";
+		$url = "http://api.openapi.io/ppurio/1/message/lms/minivertising";
+		$clientKey = "MTAyMC0xMzg3MzUwNzE3NTQ3LWNlMTU4OTRiLTc4MGItNDQ4MS05NTg5LTRiNzgwYjM0ODEyYw==";
+		$contentType = "Content-Type: application/x-www-form-urlencoded";
+	
+		$response = sendRequest($httpmethod, $url, $clientKey, $contentType, $phone, $s_url);
+
+		//echo("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
+		$json_data = json_decode($response, true);
+
+		//print_r($json_data);
+		/*
+		받아온 결과값을 DB에 저장 및 Variation
+		*/
+		$query3 = "INSERT INTO ".$_gl['sms_info_table']."(send_phone, send_status, cmid, send_regdate) values('".$phone."','".$json_data['result_code']."','".$json_data['cmid']."','".date("Y-m-d H:i:s")."')";
+		$result3 		= mysqli_query($my_db, $query3);
+
+		$query2 = "UPDATE ".$_gl['winner_info_table']." SET mb_lms='Y' WHERE mb_phone='".$phone."'";
+		$result2 		= mysqli_query($my_db, $query2);
+
+		if ($json_data['result_code'] == "200")
+			$flag = "Y";
+		else
+			$flag = "N";
+
+		return $flag;
+	}
+
+	function sendRequest($httpMethod, $url, $clientKey, $contentType, $phone, $s_url) {
+
+		//create basic authentication header
+		$headerValue = $clientKey;
+		$headers = array("x-waple-authorization:" . $headerValue);
+
+		$params = array(
+			'send_time' => '', 
+			'send_phone' => '0800237007', 
+			'dest_phone' => $phone, 
+			//'dest_phone' => '01030033965', 
+			//'dest_phone' => '01099111804', 
+			//'dest_phone' => '01030885731', 
+			//'dest_phone' => '01030033965', 
+			'send_name' => '', 
+			'dest_name' => '', 
+			'subject' => 'water bomb! bomb! 빌리프 수분폭탄 쿠폰',
+			'msg_body' => "
+빌리프 수분폭탄 이벤트에 당첨되신 것을 축하드립니다.
+
+빌리프 폭탄크림을 통해 팡팡! 터지는 촉촉함을 피부로 직접 경험해보세요.
+
+▶ 쿠폰 받기:
+".$s_url."
+▶ 교환 기간:
+4/28 ~ 6/14
+
+* 지정하신 오프라인 매장에서만 사용가능합니다.
+* 6/14이후 쿠폰 교환이 불가합니다.
+
+● 불법적인 방법으로 이벤트에 참여하신 고객님은 이벤트 당첨 대상에서 제외되며, 당첨 이후에도 당첨이 취소될 수 있습니다."
+		);
+
+		//curl initialization
+		$curl = curl_init();
+
+		//create request url
+		//$url = $url."?".$parameters;
+
+		curl_setopt ($curl, CURLOPT_URL , $url);
+		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt ($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt ($curl, CURLINFO_HEADER_OUT, true);
+		curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+
+		$response = curl_exec($curl);
+
+		$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		$responseHeaders = curl_getinfo($curl, CURLINFO_HEADER_OUT);
+
+
+		curl_close($curl);
+
+		return $response;
+	}
+
 ?>
 
 
