1588db8ff853b8871c31c66caec004a1127dd961
kyhfan <yh.kim@minivertising.kr>
2015-04-25 15:13:42 +0900

    11

--- MOBILE/winner_coupon.php
+++ MOBILE/winner_coupon.php
@@ -14,7 +14,7 @@
 		$shop_info		= BB_shop_info1($coupon_info['shop_idx']);
 		if (!$shop_info)
 		{
-			$my_db2 = mysqli_connect("192.168.124.155", "root", "m!nv#Rtisin9", "belifbomb");
+			$my_db2 = mysqli_connect("192.168.124.156", "root", "m!nv#Rtisin9", "belifbomb");
 			$coupon_info	= BB_check_serial2($serialnumber);
 
 			$shop_info		= BB_shop_info2($coupon_info['shop_idx']);
