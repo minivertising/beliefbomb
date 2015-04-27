c72ca02debbe4936d03cdad80af6fa145d659f44
kyhfan <yh.kim@minivertising.kr>
2015-04-27 13:30:39 +0900

    11

--- PC/send_sms.php
+++ PC/send_sms.php
@@ -1,5 +1,5 @@
 <?
-	$my_db = mysqli_connect("localhost", "root", "m!nv#Rtisin9", "belifbomb");
+	$my_db = mysqli_connect("192.168.124.155", "root", "m!nv#Rtisin9", "belifbomb");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
