739778b011c63cce67a1cabefa9917d906ef5df0
kyhfan <yh.kim@minivertising.kr>
2015-04-28 16:17:26 +0900

    시리얼넘버 수동 생성

--- PC/re_send_lms.php
+++ PC/re_send_lms.php
@@ -2,7 +2,8 @@
 	include_once   "./header.php";
 	$serialNumber		= BB_SerialNumber();
 
-	$query 		= "UPDATE ".$_gl['winner_info_table']." SET mb_serialnumber='".$serialNumber."' WHERE idx='1572'";
+	$idx	= $_REQUEST['idx'];
+	$query 		= "UPDATE ".$_gl['winner_info_table']." SET mb_serialnumber='".$serialNumber."' WHERE idx='".$idx."'";
 	$result 	= mysqli_query($my_db, $query);
 ?>
 
