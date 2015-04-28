53dc5bd7f8cd44583bff266cfca9d94d9f52daff
kyhfan <yh.kim@minivertising.kr>
2015-04-28 14:49:57 +0900

    당첨 확률 증가
    kit = 1/5
    miniature = 1/7

--- main_exec.php
+++ main_exec.php
@@ -92,8 +92,8 @@ switch ($_REQUEST['exec'])
 		$query3 		= "SELECT * FROM ".$_gl['winner_info_table']." WHERE mb_winner = 'miniature' AND mb_regdate like '%".date("Y-m-d")."%'";
 		$result3 	= mysqli_query($my_db, $query3);
 		$miniature_cnt	= mysqli_num_rows($result3);
-		$miniature_array = array("Y","N","N","N","N","N","N","N","N","N","N","N","N");
-		$kit_array = array("Y","N","N","N","N","N","N","N","N");
+		$miniature_array = array("Y","N","N","N","N","N","N");
+		$kit_array = array("Y","N","N","N","N");
 		//$miniature_array = array("Y","N");
 		//$kit_array = array("Y","N");
 
@@ -141,6 +141,7 @@ switch ($_REQUEST['exec'])
 		}else{
 			// 정품 당첨 추첨
 			$winner_array = array(13447,19372,23118,28494);
+			//$winner_array = array(1,2,23118,28494);
 			//$max_winner_cnt = 5;
 
 			// 오늘의 참여자 수
